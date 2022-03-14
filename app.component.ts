513bd4260a36e61a371f9a252ba636c9548f6108
SnehaBasak123 <101534960+SnehaBasak123@users.noreply.github.com>
2022-03-14 08:02:34 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+})
+
+export class AppComponent {
+  title = 'Angular4app';
+}
